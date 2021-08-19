38dfefe7b57fa3b80d19bdc404f89b8e4e820a26
Divine <dcokeyi@lakeadu.ca>
2021-08-19 19:43:39 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Card from './components/Card/card';
 
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      // residents: [],
-      // programs: [],
-    }
+function App (){
+  return(
+    <div>
+      <Card/>
+    </div>
+  )
   }
   
-
-    // componentDidMount(){
-    //   fetch('http://localhost:5000/token', {
-    //     method: 'post',
-    //     headers: {
-    //       "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify(info)
-    //   })
-    //   .then(res => res.json())
-    //   .then(res => this.setState({token: res.data.token}))
-    //   .catch(err => console(err))
-    //   }
-
-    // async componentDidMount(){
-    //  try{
-    //    let response = await axios.get('http://localhost:5000/residents')
-    //    let user = response.data
-    //    this.setState({names: user})
-    //  } catch (err) {
-    //    console.log(err)
-    //  }
-    // }
-
-    // async componentDidMount(){
-    //   try{
-    //     let response = await axios.get('http://localhost:5000/programs')
-    //     let user = response.data
-    //     this.setState({programs: user})
-    //   } catch (err) {
-    //     console.log(err)
-    //   }
-    //  }
-  render(){
-    
-
-    // const list = this.state.programs.map(n=>{
-    //   return <table>
-    //     <tr>
-    //     <th>{n.name}</th>
-    //   </tr>
-    //   </table>
-    // })
-    return(
-      <div>
-        <Card/>
-      </div>
-    )}
-  }
 
 export default App;
--- client/src/components/Card/card.js
+++ client/src/components/Card/card.js
@@ -1,74 +1,108 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 
-class Card extends Component{
-    constructor(props){
-        super(props)
-        this.state= {
-            residents: [],
-            programs: [],
-            combined:[]
-        }
+class Card extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      residents: [],
+      programs: [],
+      residentNames: []
     }
+  }
 
 
-    async componentDidMount(){
-     try{
-       let response = await axios.get('http://localhost:5000/residents')
-       let user = response.data
-       this.setState({residents: user})
-     } catch (err) {
-       console.log(err)
-     }
-     try{
-        let response = await axios.get('http://localhost:5000/programs')
-        let user = response.data
-        this.setState({programs: user})
-      } catch (err) {
-        console.log(err)
-      }
+  async componentDidMount() {
+    try {
+      let response = await axios.get('http://localhost:5000/residents')
+      let user = response.data
+      this.setState({ residents: user })
+    } catch (err) {
+      console.log(err)
     }
+    try {
+      let response = await axios.get('http://localhost:5000/programs')
+      let user = response.data
+
+
+      let ids = []
+      let innerIds = []
+
+      user.forEach(e => {
+
+        e.attendance.forEach(a => {
+          innerIds.push(a.residentId)
+        });
+
+        ids.push(innerIds);
+        innerIds = []
+
+      });
+
+      let names = []
+      let innerNames = []
+
+      ids.forEach(inId => {
+        inId.forEach(id => {
+          for (let index = 0; index < this.state.residents.length; index++) {
+            if (id === this.state.residents[index].id) {
+              innerNames.push(this.state.residents[index].name);
+              break;
+            }
+          }
+        });
+        names.push(innerNames);
+        innerNames = []
+      });
+
+      this.setState({ programs: user })
+      this.setState({ residentNames: names })
+
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
 
 
 
 
-    render(){
-       
-        return(
-            <div>
-            {/* {this.state.programs.map(n=>(
-                <table key={n.name}>
-                <thead>               
-                 <tr>
-                    <th>
-                              {n.name}
-                    </th>
-                </tr>
-                </thead>
-                {n.attendance.map((item, i)=>(
-
-                    <tbody key={i}>
-                    <tr>
-                        <td>
-                          <p>Name:{item.residentId}</p>
-                          <p>Status:{item.status}</p>
-                        </td>
-                    </tr>
-                </tbody>
-                ))}
-               
-
-            </table>
-            ))}
-            */}
-
-            </div>
-        )
+  render() {
     
-        
-    }
+    return (
+      <div>
+        {this.state.programs.map(n => (
+          <table key={n.name}>
+            <thead>
+              <tr>
+                <th>
+                  {n.name}
+                </th>
+              </tr>
+            </thead>
+            {n.attendance.map((item, i) => (
+
+              <tbody key={i}>
+                <tr>
+                  <td>
+                    <p>Name:{item.residentId}</p>
+                    <p>Status:{item.status}</p>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+
+          </table>
+        ))}
+
+
+      </div>
+    )
+
+
+  }
 
 }
-        
+
 export default Card;
